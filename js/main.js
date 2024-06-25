fd1cd18ce24d7bc60ad95e521f2bf9e8ac22b2e5
okiyamafoodservice <naotookiyama@gmail.com>
2024-06-25 12:03:59 +0900

    uchidabashi

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const menuButton = document.querySelector(".header__menuButton");
+// const menuButton = document.querySelector(".header__menuButton");
 
-console.log(menuButton);
+// console.log(menuButton);
 
-const menu = document.querySelector(".header__menu");
-console.log(menu);
+// const menu = document.querySelector(".header__menu");
+// console.log(menu);
 
-menuButton.addEventListener("click", () => {
-  menu.classList.toggle("open");
-});
+// menuButton.addEventListener("click", () => {
+//   menu.classList.toggle("open");
+// });
